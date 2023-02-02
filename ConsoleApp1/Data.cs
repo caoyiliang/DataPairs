8595da0886e697a2eb096eacf7f07ee6545fe80d
曹一梁 <caoyiliang@me.com>
2023-02-02 17:14:07 +0800

    Demo

---
+++ ConsoleApp1/Data.cs
@@ -0,0 +1,33 @@
+﻿using DataPairs;
+using DataPairs.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Data
+    {
+        public decimal A { get; set; }
+        public List<decimal> B { get; set; } = new List<decimal>();
+
+        private static readonly IDataPair<Data> _pair = new DataPair<Data>(typeof(Data).Name);
+
+        public async Task InitAsync()
+        {
+            var data = await _pair.TryGetValueAsync();
+            foreach (var item in data.GetType().GetProperties())
+            {
+                GetType().GetProperty(item.Name)!.SetValue(this, item.GetValue(data));
+            }
+        }
+
+        public async Task TrySaveChangeAsync()
+        {
+            await _pair.TryInitOrUpdateAsync(this);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Test;
+using Microsoft.Extensions.DependencyInjection;
+
+IServiceProvider serviceProvider;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddSingleton(provider => { 
+    var data=new Data();
+    data.InitAsync().Wait();
+    return data;
+});
+
+serviceProvider = serviceCollection.BuildServiceProvider();
+
+var data = serviceProvider.GetRequiredService<Data>();
+data.A = 500;
+await data.TrySaveChangeAsync();
+Console.WriteLine("Hello, World!");
